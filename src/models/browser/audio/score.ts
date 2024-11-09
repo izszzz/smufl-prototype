16f6f7a654212788fde994332e41f66fdc2f6182
izszzz <izszzz@yahoo.co.jp>
2024-11-09 23:15:57 +0900

    fix model/browser/audio

--- src/models/browser/audio/score.ts
+++ src/models/browser/audio/score.ts
@@ -1,19 +1,7 @@
 import * as Audio from ".";
 import * as Core from "../../core";
-import Soundfont2 from "../../files/soundfont2";
-export class Score {
-  core;
-  soundfont2;
-  tracks;
-  audioContext;
-  constructor(
-    core: Core.Score,
-    soundfont2: Soundfont2,
-    audioContext: AudioContext
-  ) {
-    this.core = core;
-    this.soundfont2 = soundfont2;
-    this.audioContext = audioContext;
-    this.tracks = core.tracks.map((track) => new Audio.Track(track, this));
+export class Score extends Core.Score<Audio.Note, Audio.Track> {
+  constructor(core: Core.Score<Audio.Note, Audio.Track>) {
+    super(core);
   }
 }
